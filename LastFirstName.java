e9a62b6359e8a83a979d5dd1d7382f37bacf7c45
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 20:33:49 -0500

    Add files via upload

---
+++ LastFirstName.java
@@ -0,0 +1,12 @@
+
+public class LastFirstName {
+	public static void main(String[] args){
+		String nameSid = "Sidney Seay";
+		
+		for (int i = 0; i < nameSid.length(); i++) {
+		    System.out.println(nameSid.charAt(i));
+		}
+		
+	}
+
+}
